235b095862ae88a1144080650b7229e3d5dc7165
imtbkcat <songlingyu@pingcap.com>
2018-12-17 20:55:42 +0800

    fix bug

--- format/format.go
+++ format/format.go
@@ -190,15 +190,16 @@ func OutputFormat(s string) string {
 		}
 		buf.WriteRune(old)
 	}
-	ret, raw, lim := "", buf.String(), -1
+	raw, lim := []rune(buf.String()), -1
+	buf.Reset()
 	for i := len(raw) - 1; i >= 0; i-- {
-		if raw[i] != " " {
+		if raw[i] != ' ' {
 			lim = i + 1
 			break
 		}
 	}
 	for i := 0; i < lim; i++ {
-		ret += raw[i]
+		buf.WriteRune(raw[i])
 	}
-	return ret
+	return buf.String()
 }
